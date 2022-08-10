34405e10cd53435fb5737e6a78682a34a43b364e
nagubabusf2 <110965744+nagubabusf2@users.noreply.github.com>
2022-08-10 14:36:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title("My Parents New Healthy Dinner")
+streamlit.header("Breakfast Menu")
+streamlit.text("Omea 3 & Blueverry Oatmeal")
+streamlit.text("Kale, Spinach & Rocket Smothie")
+streamlit.text("Hard-Boiled Free-Range Egg")
