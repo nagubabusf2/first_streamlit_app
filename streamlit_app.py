bbf5e67e4487ee6c59c6ec501acc025490d46d0c
nagubabusf2 <110965744+nagubabusf2@users.noreply.github.com>
2022-08-10 14:37:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 
 streamlit.title("My Parents New Healthy Dinner")
 streamlit.header("Breakfast Menu")
-streamlit.text("Omea 3 & Blueverry Oatmeal")
-streamlit.text("Kale, Spinach & Rocket Smothie")
-streamlit.text("Hard-Boiled Free-Range Egg")
+streamlit.text("🥣 Omea 3 & Blueverry Oatmeal")
+streamlit.text("🥗 Kale, Spinach & Rocket Smothie")
+streamlit.text("🐔 Hard-Boiled Free-Range Egg")
+streamlit.text("🥑🍞 Avocado Toast")
